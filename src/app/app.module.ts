3ffb62181e1cf0e0db90ff4b9006962b518b0196
ahmed <ahmed@localhost.localdomain>
2019-04-17 23:50:40 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ListenerComponent } from './smart_listener/listener/listener.component';
+import {Routes} from '@angular/router';
+const appRoutes: Routes = [
+  {path: '', component: ListenerComponent},
 
+
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListenerComponent
   ],
   imports: [
     BrowserModule
